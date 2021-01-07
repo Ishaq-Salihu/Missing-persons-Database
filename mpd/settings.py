625e02ed94a84bc764e80219437810a28167daea
Ishaq Salihu <ishaqsalihu05@gmail.com>
2021-01-07 13:47:48 +0100

    noooo

--- mpd/settings.py
+++ mpd/settings.py
@@ -162,7 +162,6 @@ REST_FRAMEWORK = {
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-os.makedirs(STATICFILES_DIRS, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True) 
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
