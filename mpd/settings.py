381e3074ee2aabd38710cda3dc3a0a65bd908f8c
Ishaq Salihu <ishaqsalihu05@gmail.com>
2021-01-07 13:23:56 +0100

    more

--- mpd/settings.py
+++ mpd/settings.py
@@ -162,6 +162,11 @@ REST_FRAMEWORK = {
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
