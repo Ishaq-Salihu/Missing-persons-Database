ee9d05df9e9b19fb977e8ded3515fb32291512b1
Ishaq Salihu <ishaqsalihu05@gmail.com>
2021-01-07 19:38:33 +0100

    a n**ga cant get a break huh

--- mpd/urls.py
+++ mpd/urls.py
@@ -27,4 +27,6 @@ urlpatterns = [
     path('api/v1/rest-auth/', include('rest_auth.urls')),
     path('api/v1/rest-auth/registration/', include('rest_auth.registration.urls')),
 
-] urlpatterns +=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
